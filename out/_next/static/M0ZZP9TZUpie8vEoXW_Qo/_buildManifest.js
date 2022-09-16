db63e8d91afa1689c60253cf79554256891d17d5
MiJin Lee <33439062+MitNy@users.noreply.github.com>
2022-09-16 18:36:03 +0900

    Delete out directory

--- out/_next/static/M0ZZP9TZUpie8vEoXW_Qo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-254f0ad79cfbf3ad7a0d.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/post/[slug]":["static/chunks/pages/post/[slug]-573a1c5b57954152da3d.js"],sortedPages:["/","/_app","/_error","/post/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
